0bb7ff3b11f9399d69d0206f36bd69d6723ec93e
potsawat <potsawat.w@ku.th>
2017-09-26 20:20:52 +0700

    Initial commit

--- src/main/java/Controllers/MainController.java
+++ src/main/java/Controllers/MainController.java
@@ -1,8 +1,10 @@
 package Controllers;
 
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
@@ -15,33 +17,39 @@ public class MainController {
     private GridPane timeGrid;
 
     @FXML
+    private Button okBtn;
+    @FXML
     public void initialize(){
+
         for (int i = 0; i < timeGrid.getRowConstraints().size(); i++) {
             for (int j = 0; j <timeGrid.getColumnConstraints().size() ; j++) {
+
                 if(j == 0 && i!=0){
 
                     timeGrid.add(new Label("STADIUM "+(i)),j,i);
                 }
                 else {
-                    final Label l = new Label(""+j);
-//                    l.setOnMouseClicked(new EventHandler<MouseEvent>() {
-//                        public void handle(MouseEvent event) {
-//                            l.getGraphic().setStyle("-fx-text-fill: #c4d8de;");
-//                        }
-//                    });
+                    final Label l = new Label("test");
+
+                    l.setPrefSize(90,70);
+                    l.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                        public void handle(MouseEvent event) {
+                            l.setStyle("-fx-background-color: cornflowerblue");
+
+                        }
+                    });
+
                     timeGrid.add(l,j,i);
-                    if(l.isMouseTransparent()){
-                        l.setOnMouseClicked(new EventHandler<MouseEvent>() {
-                            public void handle(MouseEvent event) {
-                                l.getGraphic().setStyle("-fx-text-fill: #c4d8de;");
-                            }
-                        });
-                    }
+//
                 }
+
             }
         }
 
+
     }
+
+
 }
 
 
